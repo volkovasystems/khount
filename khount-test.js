78005bd99a11aa1e30de47762239c1fa65318b66
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-05 18:55:31 +0800

    Updated test.

--- khount-test.js
+++ khount-test.js
@@ -1,7 +1,9 @@
-"use strict";
-
+const assert = require( "assert" );
 const khount = require( "./khount.js" );
 
-console.log( khount( { "name": "simple" } ) );
-console.log( khount( { } ) );
-console.log( khount( ) );
+assert.equal( khount( { "name": "simple" } ), 1, "should have value 1" );
+assert.equal( khount( [ 1, 2, 3 ] ), 4, "should have value 4" );
+assert.equal( khount( { } ), 0, "should have value 0" );
+assert.equal( khount( ), 0, "should have value 0" );
+
+console.log( "ok" );
