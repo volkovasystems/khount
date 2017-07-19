28d7b2e419653cb4927a76f253e9a97e41652ccf
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-19 21:08:28 +0800

    Updated build procedure.

--- test.module.js
+++ test.module.js
@@ -2,8 +2,11 @@ const assert = require( "assert" );
 const khount = require( "./khount.js" );
 
 assert.equal( khount( { "name": "simple" } ), 1, "should have value 1" );
+
 assert.equal( khount( [ 1, 2, 3 ] ), 4, "should have value 4" );
+
 assert.equal( khount( { } ), 0, "should have value 0" );
+
 assert.equal( khount( ), 0, "should have value 0" );
 
 console.log( "ok" );
